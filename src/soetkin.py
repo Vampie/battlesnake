3074d15b6274f8e0514620c40e0c2c4ca0025d11
peter <peter@MacBook-Pro.local>
2020-09-11 22:31:24 +0200

    fix soetkin / board refactoring

--- src/soetkin.py
+++ src/soetkin.py
@@ -5,27 +5,27 @@ class Soetkin(SnakeBrain):
   def hungry(self):
     return self.health < 25
 
-  def calculate_next_move(self, board):
+  def calculate_next_move(self):
       # for each direction calculate
       directions = {}
       for dir in self.possible_directions:
-        directions[dir] = self.get_value(board, dir)
+        directions[dir] = self.get_value(dir)
      
       new_dir = max(directions, key=directions.get)  
       return self.move_towards(new_dir)
 
-  def get_value(self, board, direction):
+  def get_value(self, direction):
     if self.is_blocked(board, direction):
       return -1000
-    head = board.cell_at(self.snake_x, self.snake_y)
-    val = board.number_of_free_cells(head, direction)
-    if (board.cells_in_direction(head, direction).can_be_occupied_in_next_round):
+    head = self.board.cell_at(self.snake_x, self.snake_y)
+    val = self.board.number_of_free_cells(head, direction)
+    if (self.board.cells_in_direction(head, direction).can_be_occupied_in_next_round):
       val = val - 20
     # print("## get value ##")
     # print(str(self.snake_x) + ", " + str(self.snake_y))
     # print(str(head.x) + ", " + str(head.y))
     
     if self.hungry():
-      d = board.distance_to_food(head, direction)
+      d = self.board.distance_to_food(head, direction)
       val = val + (20 - d)
     return val
